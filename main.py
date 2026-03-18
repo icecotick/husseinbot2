f2497b01eb1ebf58b80083e96e5da46253859b58
icecotick <dimakuzuz@gmail.com>
2026-03-18 17:14:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2382,7 +2382,7 @@ async def check_oauth_user(ctx, user: discord.User):
     
     if enemy_servers:
         enemy_text = "\n".join([f"• **{g['name']}**" for g in enemy_servers[:5]])
-             if len(enemy_servers) > 5:
+            if len(enemy_servers) > 5:
                 enemy_text.append(f"*... и ещё {len(enemy_servers) - 5}*")
             
             embed.add_field(
